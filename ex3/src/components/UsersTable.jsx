3c8dfb5a9a606783ec94d7a875939f204ef905e6
Mike Turley <mturley@hpe.com>
2017-09-16 23:35:25 -0400

    Sort users by last name, and add onRowSelection handler to table

--- ex3/src/components/UsersTable.jsx
+++ ex3/src/components/UsersTable.jsx
@@ -12,13 +12,38 @@ import {
 import Subheader from 'material-ui/Subheader';
 import { loadUsers } from '../actions/users';
 
+
 const formatAddress = address =>
   `${address.street} ${address.suite}, ${address.city}, ${address.zipcode}`;
 
+const byLastNameDesc = (userA, userB) => {
+  // Comparator for sorting users by name, descending
+  // Tries to use the last name if possible by looking after the space
+  const splitA = userA.name.split(' ');
+  const splitB = userB.name.split(' ');
+  const compareA = splitA.length === 2 ? splitA[1] : userA.name;
+  const compareB = splitB.length === 2 ? splitB[1] : userB.name;
+  if (compareA < compareB) return 1;
+  if (compareA > compareB) return -1;
+  return 0;
+};
+
 export class UsersTable extends Component {
+  constructor() {
+    super();
+    this.onRowSelection = this.onRowSelection.bind(this);
+  }
+
   componentDidMount() {
     const { dispatch } = this.props;
     dispatch(loadUsers());
+  }
+
+  onRowSelection(selection) {
+    const { users } = this.props;
+    const selectedIndex = selection[0];
+    const selectedUser = !users.loaded ? null : users.data[selectedIndex];
+    console.log('selected! ', selectedUser);
   }
 
   render() {
@@ -29,8 +54,8 @@ export class UsersTable extends Component {
     }
 
     return (
-      <Table>
-        <TableHeader>
+      <Table onRowSelection={this.onRowSelection}>
+        <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
           <TableRow>
             <TableHeaderColumn>Name</TableHeaderColumn>
             <TableHeaderColumn>Username</TableHeaderColumn>
@@ -38,9 +63,9 @@ export class UsersTable extends Component {
             <TableHeaderColumn>Address</TableHeaderColumn>
           </TableRow>
         </TableHeader>
-        <TableBody>
-          {users.data.map(user => (
-            <TableRow key={user.id}>
+        <TableBody displayRowCheckbox={false} showRowHover>
+          {users.data.sort(byLastNameDesc).map(user => (
+            <TableRow key={user.id} className="user-row">
               <TableRowColumn>{user.name}</TableRowColumn>
               <TableRowColumn>{user.username}</TableRowColumn>
               <TableRowColumn>{user.email}</TableRowColumn>
